824a40715605dd87ccc3f5827207279235162344
Drew Peters <peters.drew.dp@gmail.com>
2020-06-16 12:53:19 -0400

    updated documentation for all methods for emp-serv.

--- src/main/java/com/revature/rms/employee/controller/ResourceController.java
+++ src/main/java/com/revature/rms/employee/controller/ResourceController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/resource")
 public class ResourceController {
 
-
     private ResourceMetadataService service;
 
     @Autowired
@@ -18,45 +17,72 @@ public class ResourceController {
         this.service = service;
     }
 
-
-
+    /**
+     *
+     * @param data
+     * @return
+     */
     @PostMapping(value = "/adddata", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResourceMetadata saveData (@RequestBody ResourceMetadata data)
     {
-
         return service.save(data);
-
     }
 
+    /**
+     *
+     * @param data
+     * @return
+     */
     @PostMapping(value = "/updatemodifier", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResourceMetadata updateModifier(@RequestBody ResourceMetadata data){
         ResourceMetadata meta = service.findById(data.getResourceId());
         meta.setLastModifier(data.getLastModifier());
+
         return service.update(meta);
     }
 
+    /**
+     *
+     * @param data
+     * @return
+     */
     @PostMapping(value = "/updateall", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResourceMetadata updateAll(@RequestBody ResourceMetadata data){
         ResourceMetadata meta = service.findById(data.getResourceId());
         meta.setLastModifier(data.getLastModifier());
         meta.setResourceOwner(data.getResourceOwner());
+
         return service.update(meta);
     }
 
+    /**
+     *
+     * @param data
+     * @return
+     */
     @PostMapping(value = "/findbyid", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResourceMetadata findById(@RequestBody ResourceMetadata data){
-
         int id = data.getResourceId();
+
         return service.findById(id);
     }
 
+    /**
+     *
+     * @param data
+     * @return
+     */
     @PostMapping(value = "/findbycreator", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResourceMetadata findByCreator(@RequestBody ResourceMetadata data){
-
         int id = data.getResourceCreator();
+
         return service.findbyCreator(id);
     }
 
+    /**
+     *
+     * @return
+     */
     @GetMapping("/test")
     public @ResponseBody String test() {
         return "resourceController loaded";
--- src/main/java/com/revature/rms/employee/repositories/ResourceMetadataRepository.java
+++ src/main/java/com/revature/rms/employee/repositories/ResourceMetadataRepository.java
@@ -1,11 +1,25 @@
 package com.revature.rms.employee.repositories;
 
 import com.revature.rms.employee.entities.ResourceMetadata;
+import com.revature.rms.employee.exceptions.ResourceNotFoundException;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ResourceMetadataRepository extends CrudRepository<ResourceMetadata,Integer> {
+
+    /**
+     * findByResourceCreator method: The id parameter is passed as the input.
+     * A ResourceMetadata is returned when the input appUserId matches a database record.
+     * @param id appUserId int
+     * @return a ResourceMetadata with matching id
+     */
     ResourceMetadata findByResourceCreator(int id);
+
+    /**
+     * findByResourceId method: Returns an ResourceMetadata object when the id int matches a record in the database.
+     * @param id resourceMetadataId int
+     * @return a ResourceMetadata with matching id
+     */
     ResourceMetadata findByResourceId(int id);
 }
