d7b135129822cbf4fe1c6c8d71a2103396463f5e
Farren Springer <68714743+fjs507@users.noreply.github.com>
2020-10-14 10:41:57 -0500

    naming conventions, exception messages, etc.

--- src/main/java/com/revature/rms/employee/services/EmployeeService.java
+++ src/main/java/com/revature/rms/employee/services/EmployeeService.java
@@ -25,36 +25,86 @@ public class EmployeeService {
         this.employeeRepository = repo;
     }
 
+    /**
+     * addEmployee method: Takes in a employee object as the input. It is then added or persisted
+     * to the database.
+     *
+     * @param newEmployee newly persisted employee object
+     * @return the newly added employee object
+     */
+    @Transactional
+    public Employee save(EmployeeCreds newEmployee, int id) {
+        if(newEmployee == null){
+            throw new InvalidRequestException("New employee cannot be null!");
+        }
+        Employee employee = new Employee(newEmployee);
 
+        return employeeRepository.save(employee);
+    }
+
+    /**
+     * getall method: Returns a list of all the employee objects in the database.
+     * @return a list of all the employees
+     *
+     * @throws ResourceNotFoundException when no employees are found
+     */
+    @Transactional(readOnly = true)
+    public List<Employee> findAll() throws ResourceNotFoundException{
+        Iterable<Employee> e = employeeRepository.findAll();
+        List<Employee> list = getListFromIterator(e);
+        if (list.isEmpty()){
+            throw new ResourceNotFoundException("No employees Found!");
+        }
+        return list;
+    }
 
     /**
      * getEmployeeById method: Returns an employee object when the id int matches a record in the database.
+     *
      * @param id employeeId int value
      * @return an employee with matching id
      * @throws ResourceNotFoundException when an employee is not found
      */
     @Transactional(readOnly = true)
-    public Employee getEmployeeById(int id) throws ResourceNotFoundException{
-        if (id < 1){
-            throw new InvalidRequestException("Id number can not be below 1");
+    public Employee findById(int id) throws ResourceNotFoundException{
+        if (id <= 0){
+            throw new InvalidRequestException("ID cannot be less than or equal to zero!");
         }
         Employee employee = employeeRepository.findById(id);
         if (employee == null){
-            throw new ResourceNotFoundException("No Employee found with the id of: " + id);
+            throw new ResourceNotFoundException("No employee found with the ID: " + id);
         }
         return employee;
     }
 
     /**
+     * findByFirstName method: Returns an employee object when the firstName String matches a record in the database.
+     * @param name employeeFirstName String value
+     *
+     * @return an employee with matching firstName
+     * @throws ResourceNotFoundException when an employee is not found
+     */
+    @Transactional(readOnly = true)
+    public Employee findByFirstname(String name) throws ResourceNotFoundException{
+        Employee employee = employeeRepository.findByFirstName(name);
+        if (employee == null){
+            throw new ResourceNotFoundException("No employee found with first name: " + name);
+        }
+        return employee;
+
+    }
+
+    /**
      * findEmployeeByOwnerId method: Retrieves a list of employees based on boss' ID
+     *
      * @param id Boss' ID
      * @return List of employees
      */
     @Transactional(readOnly = true)
-    public List<Employee> findEmployeeByOwnerId(int id){
+    public List<Employee> findByOwnerId(int id){
 
-        if(id < 1){
-            throw new InvalidRequestException("Id number can not be below 1");
+        if(id <= 0){
+            throw new InvalidRequestException("ID cannot be less than or equal to zero!");
         }
 
         Iterable<Employee> allEmps = employeeRepository.findAll();
@@ -69,7 +119,7 @@ public class EmployeeService {
         }
 
         if(emps.isEmpty()){
-            throw new ResourceNotFoundException("No Employees found with the Owner id of: " + id);
+            throw new ResourceNotFoundException("No employees found with the owner ID: " + id);
         }
 
         return emps;
@@ -79,6 +129,7 @@ public class EmployeeService {
     /**
      * update method: The employee object is inputted and changes are saved.
      * The modified object is returned.
+     *
      * @param updatedEmp newly updated employee object
      * @return updated/modified employee object
      */
@@ -87,7 +138,7 @@ public class EmployeeService {
         Employee emp = new Employee(updatedEmp);
         Employee oldEmp = employeeRepository.findById(emp.getId());
         if (oldEmp == null){
-            throw new ResourceNotFoundException("No Employee exists with id of:" + updatedEmp.getId());
+            throw new ResourceNotFoundException("No employee exists with ID:" + updatedEmp.getId());
         }
         ResourceMetadata metadata = oldEmp.getResourceMetadata();
         metadata.setLastModifier(id);
@@ -98,54 +149,25 @@ public class EmployeeService {
     }
 
     /**
-     * addEmployee method: Takes in a employee object as the input. The input employee
-     * object is tested to ensure that it is not null. If the employee object
-     * is null then it will throw a Invalid Request Exception.
-     * Once the employee object passes the test it is then added or persisted
-     * to the database.
-     * @param newEmployee newly persisted employee object
-     * @return the newly added employee object
+     * delete method: Deletes an employee object based on its id int
+     *
+     * @param id employeeId int value
      */
     @Transactional
-    public Employee addEmployee(EmployeeCreds newEmployee, int id) {
-        if(newEmployee == null){
-            throw new InvalidRequestException("A new Employee must be entered in order for a new Employee to be saved");
+    public void delete(int id) {
+        if (id <= 0) {
+            throw new InvalidRequestException("ID cannot be less than or equal to zero!");
         }
-        Employee employee = new Employee(newEmployee);
-
-        return employeeRepository.save(employee);
+        employeeRepository.deleteById(id);
     }
 
     /**
-     * findByFirstName method: Returns an employee object when the firstName String matches a record in the database.
-     * @param name employeeFirstName String value
-     * @return an employee with matching firstName
-     * @throws ResourceNotFoundException when an employee is not found
+     * Convenience Method
+     *
+     * @param iterable
+     * @param <T>
+     * @return
      */
-    @Transactional(readOnly = true)
-    public Employee findByFirstname(String name) throws ResourceNotFoundException{
-        Employee employee = employeeRepository.findByFirstName(name);
-        if (employee == null){
-            throw new ResourceNotFoundException("No Employee found with first name: " + name);
-        }
-        return employee;
-
-    }
-
-    /**
-     * getall method: Returns a list of all the employee objects in the database.
-     * @return a list of all the employees
-     * @throws ResourceNotFoundException when no employees are found
-     */
-    @Transactional(readOnly = true)
-    public List<Employee> getAll() throws ResourceNotFoundException{
-        Iterable<Employee> e = employeeRepository.findAll();
-        List<Employee> list = getListFromIterator(e);
-        if (list.isEmpty()){
-            throw new ResourceNotFoundException("No Employees Found!");
-        }
-        return list;
-    }
     public static <T> List<T> getListFromIterator(Iterable<T> iterable)
     {
         List<T> list = new ArrayList<>();
@@ -153,15 +175,5 @@ public class EmployeeService {
         return list;
     }
 
-    /**
-     * delete method: Deletes an employee object based on its id int
-     * @param id employeeId int value
-     */
-    @Transactional
-    public void delete(int id) {
-        if (id <= 0) {
-            throw new InvalidRequestException("Id number can not be below 0");
-        }
-        employeeRepository.deleteById(id);
-    }
+
 }
