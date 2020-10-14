417819fbaefe6d5818ad1f530d85dd3eb15f3655
Stephen Brasel <wearyroad@gmail.com>
2020-10-14 16:50:14 -0400

    refactored resourcemetadata out of employee

--- src/test/java/com/revature/rms/employee/repositories/EmployeeRepositoryTest.java
+++ src/test/java/com/revature/rms/employee/repositories/EmployeeRepositoryTest.java
@@ -3,7 +3,7 @@ package com.revature.rms.employee.repositories;
 import com.revature.rms.employee.dtos.EmployeeCreds;
 import com.revature.rms.employee.entities.Department;
 import com.revature.rms.employee.entities.Employee;
-import com.revature.rms.employee.entities.ResourceMetadata;
+import com.revature.rms.core.metadata.ResourceMetadata;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
